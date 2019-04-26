03154868aba7c55362407851451947f5282ad741
skylock <emanuil.copil@gmail.com>
2019-04-26 21:26:36 +0300

    Update challenge#01.js

--- challenge#01.js
+++ challenge#01.js
@@ -1,31 +1,26 @@
 {
     init: (elevators, floors) => {
         let elevator = elevators[0] // Let's use the first elevator
-        let firstFloor = floors[0];
-        let lastFloor = floors[2];
+        let midleFloor = Math.floor(floors.length / 2);
 
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", () => {
             // let's go to the middle of the building
-            let midleFloor = Math.floor(floors.length / 2);
-            elevator.goToFloor(midleFloor);
+            if(elevator.currentFloor() !== 0)
+                elevator.goToFloor(midleFloor);         
         });
-        
+
         elevator.on('floor_button_pressed', floorNum => {
             elevator.goToFloor(floorNum);
         })
         
-        firstFloor.on("up_button_pressed", () => {
-            // Maybe tell an elevator to go to this floor?
-            elevator.goToFloor(firstFloor.floorNum());
-        })
-        
-        lastFloor.on("down_button_pressed", () => {
-            // Maybe tell an elevator to go to this floor?
-            elevator.goToFloor(lastFloor.floorNum());
-        })
+        for(let i=0; i < floors.length; i++) {
+            floors[i].on("up_button_pressed", () => {
+                elevator.goToFloor(floors[i].floorNum());
+            })
+        }
     },
-    update: (dt, elevators, floors) => {
-        // We normally don't need to do anything here
-    }
+        update: (dt, elevators, floors) => {
+            // We normally don't need to do anything here
+        }
 }
