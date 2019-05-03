d60ad32cdf37dcc609d3f10a83da65235e84fd42
skylock <emanuil.copil@gmail.com>
2019-05-03 12:44:47 +0300

    Update challenge#01.js

--- challenge#01.js
+++ challenge#01.js
@@ -1,26 +1,41 @@
 {
-    init: (elevators, floors) => {
-        let elevator = elevators[0] // Let's use the first elevator
-        let midleFloor = Math.floor(floors.length / 2);
-
+    init: function(elevators, floors) {
+        let elevator = elevators[0]; // Let's use the first elevator
+        let floorCallQueue = [];
+        
+        initFloorButtonsListeners();
+        
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", () => {
-            // let's go to the middle of the building
-            if(elevator.currentFloor() !== 0)
-                elevator.goToFloor(midleFloor);         
+            log('elevator is idle');
         });
-
-        elevator.on('floor_button_pressed', floorNum => {
-            elevator.goToFloor(floorNum);
-        })
         
-        for(let i=0; i < floors.length; i++) {
-            floors[i].on("up_button_pressed", () => {
-                elevator.goToFloor(floors[i].floorNum());
-            })
+        elevator.on('floor_button_pressed ', floorNum => {
+            elevator.goToFloor(floorNum, true);
+        });
+        
+        elevator.on("stopped_at_floor", floorNum => {
+        });
+        
+        function log(message) {
+            console.log.apply(message, arguments);
+        }
+        
+        function initFloorButtonsListeners() {
+            for (let i = 0; i < floors.length; i++) {
+                let floor = floors[i];
+                floor.on('up_button_pressed ', () => {
+                    //floorCallQueue.push({floorNum: floor.floorNum(), direction: 'up'});
+                    elevator.goToFloor(floor.floorNum());
+                });
+                floor.on('down_button_pressed ', () => {
+                    //floorCallQueue.push({floorNum: floor.floorNum(), direction: 'down'});
+                    elevator.goToFloor(floor.floorNum());
+                });
+            }
         }
     },
-        update: (dt, elevators, floors) => {
-            // We normally don't need to do anything here
-        }
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
